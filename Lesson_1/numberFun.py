53e48c826ad93b67f427d2b2636681becee45766
awmaxwell144 <awmaxwell144@gmail.com>
2022-01-23 20:19:17 -0500

    first

---
+++ Lesson_1/numberFun.py
@@ -0,0 +1,28 @@
+
+import random
+
+number1 = random.randint(1,100)
+number2 = random.randint(1,100)
+print("First number = ", number1)
+print("First number = ", number2)
+print(str(number1) + " / " + str(number2) + " = " + str(number1 / number2))
+print(str(number1) + " // " + str(number2) + " = " + str(number1 // number2))
+print(str(number1) + " % " + str(number2) + " = " + str(number1 % number2))
+print(str(number1) + " + " +
+ str(number2) + " = " + str(number1 + number2))
+print(str(number1) + " - " + str(number2) + " = " + str(number1 - number2))
+print(str(number1) + " * " + str(number2) + " = " + str(number1 * number2))
+print()
+
+
+NUMBER2 = 25
+number2 = NUMBER2
+print("First number = ", number1)
+print("First number = ", number2)
+print(str(number1) + " / " + str(number2) + " = " + str(number1 / number2))
+print(str(number1) + " // " + str(number2) + " = " + str(number1 // number2))
+print(str(number1) + " % " + str(number2) + " = " + str(number1 % number2))
+print(str(number1) + " + " + str(number2) + " = " + str(number1 + number2))
+print(str(number1) + " - " + str(number2) + " = " + str(number1 - number2))
+print(str(number1) + " * " + str(number2) + " = " + str(number1 * number2))
+print()
---
+++ Lesson_1/stringFun.py
@@ -0,0 +1,24 @@
+#exercise 1 
+print("--------------------------------------------")
+print("Exercise 1")
+name = "alexandra maxwell"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+#exercise 2
+print("--------------------------------------------")
+print("Exercise 2")
+noun = "cat"
+adj = "black"
+verb = "napped"
+ending = "by the window"
+sentence1 = "the " + " little " + adj + " " + noun + " " + verb + " " + ending
+sentence2 = f"the little {adj} {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+
+
+
